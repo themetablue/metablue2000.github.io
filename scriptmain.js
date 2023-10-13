9388d308dee534a08444a395a2ade2e3b7092723
MetaBlue <108994181+MetaBlue2000@users.noreply.github.com>
2023-10-13 16:18:13 +0200

    master

--- scriptmain.js
+++ scriptmain.js
@@ -1,10 +1,5 @@
-// Fonction pour recharger la page toutes les 15 secondes
 function rechargerPage() {
     location.reload();
 }
-
-// Appeler la fonction pour la première fois
 rechargerPage();
-
-// Configurer une boucle pour recharger la page toutes les 15 secondes
 setInterval(rechargerPage, 3000);
